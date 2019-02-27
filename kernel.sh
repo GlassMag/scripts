8a9c63df7180fc2379dfbcb77a4c388bb6c56e91
ProtoChuz <47939304+ProtoChuz@users.noreply.github.com>
2019-02-27 09:23:46 +0700

    TES

--- kernel.sh
+++ kernel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-git clone git://github.com/krasCGQ/aarch64-linux-android -b a53-7.x --depth=1 gcc
+wgetx https://releases.linaro.org/components/toolchain/binaries/latest-7/aarch64-linux-gnu/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
+tar xvf gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu.tar.xz
 make clean distclean mrproper
 export ARCH=arm64
-export CROSS_COMPILE=/home/runner/CrappyKernel/gcc/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/runner/CrappyKernel/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 export KBUILD_BUILD_USER=ProtoChuz
 export KBUILD_BUILD_HOST=SemaphoreCI
 export USE_CCACHE=1

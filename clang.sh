7f95c076e7d40e61a602280f2c98892934902207
GlassMag <48284684+GlassMag@users.noreply.github.com>
2019-03-09 14:29:35 +0700

    Create clang.sh

---
+++ clang.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+git clone git://github.com/krasCGQ/aarch64-linux-android -b a53-7.x --depth=1 gcc
+mkdir celang
+cd celang
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/master/clang-r328903.tar.gz 
+tar -xvf clang-r328903.tar.gz
+cd ..
+make clean distclean mrproper
+export ARCH=arm64
+export CC=/home/runner/CrappyKernel/celang/bin/clang
+export CLANG_TRIPLE=/home/runner/CrappyKernel/gcc/bin/aarch64-cortex_a53-linux-android-
+export CLANG_LD_PATH=/home/runner/CrappyKernel/clang/lib
+export LLVM_DIS=/home/runner/CrappyKernel/celang/bin/llvm-dis
+
+export KBUILD_BUILD_USER=ProtoChuz
+export KBUILD_BUILD_HOST=SemaphoreCI
+export USE_CCACHE=1
+export CACHE_DIR=~/.ccache
+tanggal=$(date +'%m%d-%H%M')
+curl -F chat_id="-1001415832052" -F parse_mode="HTML" -F text="Building <b>CrappyKernel Liquid</b>
+Compiler : <code>Linaro GCC 7.3 & Clang</code>
+Last Commit : <code>$(git log --oneline --decorate --color --pretty=%s --first-parent -1)</code>
+Build Started on : <code>$(date)</code>
+Build using : <code>SemaphoreCI</code>" https://api.telegram.org/bot757761074:AAFKxcBRT-hsNfyC0wXTH_GXJozT7yzflKU/sendMessage
+rm -rf output
+mkdir output
+START=$(date +"%s");
+make -C $(pwd) O=output santoni_nontreble_defconfig
+make -j$(nproc --all) -C $(pwd) O=output 2>&1| tee ${tanggal}-Log.txt
+if [ ! -f output/arch/arm64/boot/Image.gz-dtb ]; then
+    echo "HolyCrap, Compiling Failed"
+    curl -F chat_id="-1001324692867" -F document=@"${tanggal}-Log.txt" https://api.telegram.org/bot757761074:AAFKxcBRT-hsNfyC0wXTH_GXJozT7yzflKU/sendDocument
+    curl -F chat_id="-1001415832052" -F text="Build throw an error(s)" https://api.telegram.org/bot757761074:AAFKxcBRT-hsNfyC0wXTH_GXJozT7yzflKU/sendMessage
+    curl -F chat_id="-1001415832052" -F sticker="CAADBQADlAADcX38FOe-kEXhrShYAg" https://api.telegram.org/bot757761074:AAFKxcBRT-hsNfyC0wXTH_GXJozT7yzflKU/sendSticker
+
+else 
+cp output/arch/arm64/boot/Image.gz AnyKernel2/kernel/Image.gz
+
+cp output/arch/arm64/boot/dts/qcom/msm8940-pmi8950-qrd-sku7_S88536AA2.dtb AnyKernel2/nontreble/msm8940-pmi8950-qrd-sku7_S88536AA2.dtb
+
+cp output/arch/arm64/boot/dts/qcom/msm8940-pmi8950-qrd-sku7_S88536AA2-treble.dtb AnyKernel2/treble/msm8940-pmi8950-qrd-sku7_S88536AA2-treble.dtb
+
+cd AnyKernel2
+rm -rf *.zip
+zip -r9 CrappyKernel-Liquid-${tanggal}.zip * -x README.md CrappyKernel-Liquid--${tanggal}.zip
+curl -F chat_id="-1001415832052" -F document=@"CrappyKernel-Liquid-${tanggal}.zip" https://api.telegram.org/bot757761074:AAFKxcBRT-hsNfyC0wXTH_GXJozT7yzflKU/sendDocument
+END=$(date +"%s")
+DIFF=$(($END - $START))
+curl -F chat_id="-1001415832052" -F text="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds." https://api.telegram.org/bot757761074:AAFKxcBRT-hsNfyC0wXTH_GXJozT7yzflKU/sendMessage
+curl -F chat_id="-1001415832052" -F sticker="CAADBQADkwADcX38FOQ9JIMIFhnQAg" https://api.telegram.org/bot757761074:AAFKxcBRT-hsNfyC0wXTH_GXJozT7yzflKU/sendSticker
+
+fi
